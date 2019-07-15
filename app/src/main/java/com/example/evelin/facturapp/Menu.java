7b4e39ac6cefda882ec8b25d2d664937da438555
Evelin Jimenez Ramirez <lumiel801@gmail.com>
2019-07-15 11:56:42 -0500

    Vista nuevo cliente

--- app/src/main/java/com/example/evelin/facturapp/Menu.java
+++ app/src/main/java/com/example/evelin/facturapp/Menu.java
@@ -1,7 +1,9 @@
 package com.example.evelin.facturapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+    public void nuevocliente (View view){
+        startActivity(new Intent(this,Menu_cliente.class));
     }
 }
